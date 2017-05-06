c257bc6a1482e43f5e1e0b074fd12015a33c7c4a
Sargy <lukina_sargy@mail.ru>
2017-05-06 20:57:58 +0300

    contacts xs2

--- src/js/contacts.js
+++ src/js/contacts.js
@@ -1,32 +1,32 @@
 $(document).ready(function() {
-  $("#link:eq(0)").on("click", function(event) {
+  $(".contacts__link:eq(0)").on("click", function(event) {
     event.preventDefault();
     $(".manager:eq(0)").show();
     $(".contacts__countries").show();
   });
 
-  $("#link:eq(1)").one("click", function(event) {
+  $(".contacts__link:eq(1)").one("click", function(event) {
     event.preventDefault();
     $(".contacts__countries").show(); 
     $(".manager:eq(1)").show();
   });
 
-  $("#link:eq(2)").on("click", function(event) {
+  $(".contacts__link:eq(2)").on("click", function(event) {
     event.preventDefault();
     $(".contacts__countries").show(); 
     $(".manager:eq(2)").show();
   });
-  $("#link:eq(3)").on("click", function(event) {
+  $(".contacts__link:eq(3)").on("click", function(event) {
     event.preventDefault();
     $(".contacts__countries").show(); 
     $(".manager:eq(3)").show();
      });
-  $("#link:eq(4)").on("click", function(event) {
+  $(".contacts__link:eq(4)").on("click", function(event) {
     event.preventDefault();
     $(".contacts__countries").show(); 
     $(".manager:eq(4)").show();
      });
-  $("#link:eq(5)").on("click", function(event) {
+  $(".contacts__link:eq(5)").on("click", function(event) {
     event.preventDefault();
     $(".contacts__countries").show(); 
     $(".manager:eq(5)").show();
