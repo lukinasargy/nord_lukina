5a3a01f40cb59a2f85320fc423ca15eced574acd
Sargy <lukina_sargy@mail.ru>
2017-05-06 23:40:41 +0300

    contacts

--- src/js/contacts.js
+++ src/js/contacts.js
@@ -1,37 +1,11 @@
 $(document).ready(function() {
 
-  $(".contacts__link").on("click", function(event) {
+ $(".contacts__link").on("click", function(event) {
     event.preventDefault();
-   $( $(this.hash) ).show();
     $(".contacts__countries").show();
+    $(".manager").hide();    
+    $( $(this.hash) ).show();
   });
-
-  // $(".contacts__link:eq(1)").one("click", function(event) {
-  //   event.preventDefault();
-  //   $(".contacts__countries").show(); 
-  //   $(".manager:eq(1)").show();
-  // });
-
-  // $(".contacts__link:eq(2)").on("click", function(event) {
-  //   event.preventDefault();
-  //   $(".contacts__countries").show(); 
-  //   $(".manager:eq(2)").show();
-  // });
-  // $(".contacts__link:eq(3)").on("click", function(event) {
-  //   event.preventDefault();
-  //   $(".contacts__countries").show(); 
-  //   $(".manager:eq(3)").show();
-  //    });
-  // $(".contacts__link:eq(4)").on("click", function(event) {
-  //   event.preventDefault();
-  //   $(".contacts__countries").show(); 
-  //   $(".manager:eq(4)").show();
-  //    });
-  // $(".contacts__link:eq(5)").on("click", function(event) {
-  //   event.preventDefault();
-  //   $(".contacts__countries").show(); 
-  //   $(".manager:eq(5)").show();
-  //    });
 
   $(".manager__close").on("click",function(){
         $(this).closest(".contacts__countries").hide();
@@ -42,7 +16,7 @@ $(document).ready(function() {
          $(this).closest(".contacts__countries").hide();
       }
     });
-  function closeModal() {
-    $(this).closest(".manager").hide();
-  }
+  // function closeModal() {
+  //   $(this).closest(".contacts__countries").hide();
+  // }
 });
