9fe3aac2d1a4bca58eb9f1b01c78d0387321883d
Sargy <lukina_sargy@mail.ru>
2017-05-06 21:06:36 +0300

    contacts xs3

--- src/js/contacts.js
+++ src/js/contacts.js
@@ -1,36 +1,37 @@
 $(document).ready(function() {
-  $(".contacts__link:eq(0)").on("click", function(event) {
+
+  $(".contacts__link").on("click", function(event) {
     event.preventDefault();
-    $(".manager:eq(0)").show();
+   $( $(this.hash) ).show();
     $(".contacts__countries").show();
   });
 
-  $(".contacts__link:eq(1)").one("click", function(event) {
-    event.preventDefault();
-    $(".contacts__countries").show(); 
-    $(".manager:eq(1)").show();
-  });
+  // $(".contacts__link:eq(1)").one("click", function(event) {
+  //   event.preventDefault();
+  //   $(".contacts__countries").show(); 
+  //   $(".manager:eq(1)").show();
+  // });
 
-  $(".contacts__link:eq(2)").on("click", function(event) {
-    event.preventDefault();
-    $(".contacts__countries").show(); 
-    $(".manager:eq(2)").show();
-  });
-  $(".contacts__link:eq(3)").on("click", function(event) {
-    event.preventDefault();
-    $(".contacts__countries").show(); 
-    $(".manager:eq(3)").show();
-     });
-  $(".contacts__link:eq(4)").on("click", function(event) {
-    event.preventDefault();
-    $(".contacts__countries").show(); 
-    $(".manager:eq(4)").show();
-     });
-  $(".contacts__link:eq(5)").on("click", function(event) {
-    event.preventDefault();
-    $(".contacts__countries").show(); 
-    $(".manager:eq(5)").show();
-     });
+  // $(".contacts__link:eq(2)").on("click", function(event) {
+  //   event.preventDefault();
+  //   $(".contacts__countries").show(); 
+  //   $(".manager:eq(2)").show();
+  // });
+  // $(".contacts__link:eq(3)").on("click", function(event) {
+  //   event.preventDefault();
+  //   $(".contacts__countries").show(); 
+  //   $(".manager:eq(3)").show();
+  //    });
+  // $(".contacts__link:eq(4)").on("click", function(event) {
+  //   event.preventDefault();
+  //   $(".contacts__countries").show(); 
+  //   $(".manager:eq(4)").show();
+  //    });
+  // $(".contacts__link:eq(5)").on("click", function(event) {
+  //   event.preventDefault();
+  //   $(".contacts__countries").show(); 
+  //   $(".manager:eq(5)").show();
+  //    });
 
   $(".manager__close").on("click",function(){
         $(this).closest(".contacts__countries").hide();
