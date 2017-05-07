8694849aa0d3d04c464358f5e63e43d8d699d8ea
Sargy <lukina_sargy@mail.ru>
2017-05-08 00:43:35 +0300

    tabs end

--- src/js/price.js
+++ src/js/price.js
@@ -1,12 +1,26 @@
 $(document).ready(function(){
     $(".price-tabs__content").first().show()
-    // $(".price-tabs__content").hide();
+
+    $(".price-tabs--active").on("click", function(event){
+
+      event.preventDefault()
+      $(this).addClass("price-tabs--active")
+      .removeClass("price-tabs__link")
+      .removeClass("price-tabs__link")
+      .siblings().removeClass("price-tabs--active")
+      .addClass("price-tabs__link")
+      $($(this.hash)).show()
+
+      .siblings(".price-tabs__content").hide();
+    });
+
     $(".price-tabs__link").on("click", function(event){
       event.preventDefault()
       $(this).addClass("price-tabs--active")
+      .removeClass("price-tabs__link")
       .siblings().removeClass("price-tabs--active")
+      .addClass("price-tabs__link")
     $($(this.hash)).show()
-    	.append ($(this.hash))
       .siblings(".price-tabs__content").hide();
     });
 });
