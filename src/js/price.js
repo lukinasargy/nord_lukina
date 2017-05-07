48f4635475bbe47d8ec48abc8a6f431d31c95019
Sargy <lukina_sargy@mail.ru>
2017-05-07 21:05:19 +0300

    tabs middle

--- src/js/price.js
+++ src/js/price.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
-    
-    $(".price-tabs__content").hide();
+    $(".price-tabs__content").first().show()
+    // $(".price-tabs__content").hide();
     $(".price-tabs__link").on("click", function(event){
       event.preventDefault()
+      $(this).addClass("price-tabs--active")
+      .siblings().removeClass("price-tabs--active")
     $($(this.hash)).show()
     	.append ($(this.hash))
       .siblings(".price-tabs__content").hide();
